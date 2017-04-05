d03aeb433740a9c46acd772e8e319e10da15e7b9
Yuvan Wickramasinghe <ywickram@usc.edu>
2017-04-04 17:35:38 -0700

    Added user skeleton.

--- fiendFray/src/base/user.java
+++ fiendFray/src/base/user.java
@@ -1,5 +1,68 @@
 package base;
 
-public class user {
+import java.util.ArrayList;
 
+public class user {
+	private String username;
+	private String password;
+	private Pet userPet;
+	private int gems;
+	private boolean isGuest;
+	
+	public user(String username, String password, Pet userPet){
+		this.username = username;
+		this.password = password;
+		this.userPet = userPet;
+		this.gems = 10;
+		this.isGuest = false;
+	}
+	
+	public user() {
+		// TODO: add automatic guest name
+		this.username = "Guest";
+		this.password = "";
+		this.userPet = new Pet();
+		this.gems = 10;
+		this.isGuest = true;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Pet getUserPet() {
+		return userPet;
+	}
+
+	public void setUserPet(Pet userPet) {
+		this.userPet = userPet;
+	}
+
+	public int getGems() {
+		return gems;
+	}
+
+	public void setGems(int gems) {
+		this.gems = gems;
+	}
+
+	public boolean isGuest() {
+		return isGuest;
+	}
+
+	public void setGuest(boolean isGuest) {
+		this.isGuest = isGuest;
+	}
 }

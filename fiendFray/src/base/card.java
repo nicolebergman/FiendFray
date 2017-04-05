09e10dcc79f2a61bfd9d960958d433a5418dbe34
Nicole Bergman <njbergma@usc.edu>
2017-04-04 17:03:20 -0700

    Add classes 'weapon' and 'card'

--- fiendFray/src/base/card.java
+++ fiendFray/src/base/card.java
@@ -1,5 +1,37 @@
 package base;
 
+import java.util.Random;
+
 public class card {
+	
+	private String info;
+	private String imgURL; 
+	
+	card() {
+		
+	}
+	
+	public String getCardInfo() {
+		return info;
+	}
+	
+	public void setCardInfo(String info) {
+		this.info = info;
+	}
+	
+	public String getImgURL() {
+		return imgURL;
+	}
+	
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+	
+	public int randomCardValue() { //RNG to get a random card 
+		// to do
+		Random rand = new Random(); 
+		int value = rand.nextInt(50);	// max is 50
+		return value;
+	}
 
 }
--- fiendFray/src/base/weapon.java
+++ fiendFray/src/base/weapon.java
@@ -1,5 +1,37 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class weapon {
+	private String imgURL;
+	private int damage;
+	private HashMap<String, Integer> handToDamage;
+	
+	weapon() {
+		setHandToDamage(new HashMap<String, Integer>());
+	}
+	
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+	
+	public String getImgURL() {
+		return imgURL;
+	}
+	
+	public int returnDamage(ArrayList<String> pokerHand) {	//returns a number based on pokerHand
+		// to do 	
+		return damage;
+	}
+
+	public HashMap<String, Integer> getHandToDamage() {
+		return handToDamage;
+	}
+
+	public void setHandToDamage(HashMap<String, Integer> handToDamage) {
+		this.handToDamage = handToDamage;
+	}
+
 
 }

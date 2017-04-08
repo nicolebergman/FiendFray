bd734e8aa19b3214babc8b87629347c5884d718b
Nicole Bergman <njbergma@usc.edu>
2017-04-08 13:50:38 -0700

    Add files via upload

---
+++ fiendFray/src/base/stringConstants.java
@@ -0,0 +1,20 @@
+package base;
+
+public class stringConstants {
+	
+	//user fields
+	public static final String USERNAME = "username";
+	public static final String PASSWORD = "password";
+
+	//servlets
+	public static final String FILE_CHOOSER_SERVLET = "/FileChooserServlet";
+	public static final String LOGIN_SERVLET = "/loginServlet";
+	public static final String SIGN_UP_SERVLET = "/signUpServlet";
+
+	//jsp and html used strings
+	public static final String DATA = "data";
+	public static final String ERROR = "error";
+	public static final String INFILE = "infile";
+	public static final String FULL_NAME = "fullname";
+	public static final String JSP_EXT = "jsp/";
+}

495d8fcbc2735f25ce2403c39615d767e6ad329a
Nickymonkey <nickyroewe@mac.com>
2017-04-08 14:59:52 -0700

    added parser.java - Nick Roewe

---
+++ fiendFray/src/base/parser.java
@@ -0,0 +1,14 @@
+package base;
+
+import java.util.ArrayList;
+
+public class parser {
+	ArrayList<battle> currentBattles;
+	ArrayList<user> allUsers;
+	ArrayList<weapon> allWeapons;
+	shop currentShop;
+	
+	public parser(){
+		
+	}
+}

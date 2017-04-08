2828497e0783688064b298cee0cc8da46ac552f4
Nicole Bergman <njbergma@usc.edu>
2017-04-08 13:21:55 -0700

    constants - Nicole

---
+++ fiendFray/src/base/StringConstants.java
@@ -0,0 +1,22 @@
+package base;
+
+public class StringConstants {
+	
+	//user fields
+	public static final String USERNAME = "username";
+	public static final String PASSWORD = "password";
+
+	//servlets
+	public static final String FILE_CHOOSER_SERVLET = "/FileChooserServlet";
+	public static final String LOGIN_SERVLET = "/LoginServlet";
+	public static final String SIGN_UP_SERVLET = "/SignUpServlet";
+	public static final String SEARCH_SERVLET = "/SearchServlet";
+
+	//jsp and html used strings
+	public static final String DATA = "data";
+	public static final String ERROR = "error";
+	public static final String INFILE = "infile";
+	public static final String FULL_NAME = "fullname";
+	public static final String JSP_EXT = "jsp/";
+	public static final String LOGIN_JSP = "login.jsp";
+}

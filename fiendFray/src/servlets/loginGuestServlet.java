854259b14fb23525c09959cf10fd99a4eab93671
Nickymonkey <nickyroewe@mac.com>
2017-04-09 16:47:39 -0700

    fixed project breaking error - Nick Roewe

--- fiendFray/src/servlets/loginGuestServlet.java
+++ fiendFray/src/servlets/loginGuestServlet.java
@@ -20,7 +20,7 @@ import base.user;
 import server.MySQLDriver;
 
 
-@WebServlet("/loginServlet")
+@WebServlet("/loginGuestServlet")
 public class loginGuestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 

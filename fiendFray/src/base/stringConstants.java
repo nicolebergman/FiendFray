dcaa1d40e972a306ef2c68ceaca965aee28ab834
Nicole Bergman <njbergma@usc.edu>
2017-04-10 15:13:37 -0700

    Added missing constants - Nicole

--- fiendFray/src/base/stringConstants.java
+++ fiendFray/src/base/stringConstants.java
@@ -10,7 +10,6 @@ public class stringConstants {
 	public static final String PETIMAGEURL = "petImageURL";
 
 	//servlets
-	public static final String FILE_CHOOSER_SERVLET = "/FileChooserServlet";
 	public static final String LOGIN_SERVLET = "/loginServlet";
 	public static final String SIGN_UP_SERVLET = "/signUpServlet";
 	public static final String GUEST_LOGIN_SERVLET = "/loginGuestServlet";

4246b3e364f250154d003086fb416372de4033a2
Nickymonkey <nickyroewe@mac.com>
2017-04-16 12:38:19 -0700

    duplicate exp functions - Nick Roewe

--- fiendFray/src/base/pet.java
+++ fiendFray/src/base/pet.java
@@ -14,12 +14,6 @@ public class pet {
 	private weapon equippedWeapon; 
 	private int weaponID;
 	
-	public void setCurrentEXP(int currentEXP) {
-		this.currentEXP = currentEXP;
-	}
-	public void setRequiredEXPToLevelUp(int requiredEXPToLevelUp) {
-		this.requiredEXPToLevelUp = requiredEXPToLevelUp;
-	}
 	public void setEquippedWeapon(weapon equippedWeapon) {
 		this.equippedWeapon = equippedWeapon;
 	}
